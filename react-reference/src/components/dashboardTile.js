36bfa8eae9e7376fc740b4b0984e273429020eed
Rebekah Sunmi Bae <rebekahsunmibae@Rebekahs-MacBook-Pro.local>
2017-10-26 12:03:16 -0400

    Added styling to dashboard tile hover event and callback

--- react-reference/src/components/dashboardTile.js
+++ react-reference/src/components/dashboardTile.js
@@ -3,13 +3,13 @@ import { BrowserRouter, Route, Link, Redirect, Switch } from "react-router-dom";
 import "../App.css";
 
 class DashboardTile extends Component {
-	constructor(props) {
-		super(props);
+	constructor() {
+		super();
 	}
 	render() {
 		return (
-			<div className="Tile">
-				<p className="TileText">A Topic</p>
+			<div className="Tile" onMouseEnter={this.props.onMouseEnter}>
+				<p className="TileText">{this.props.tileText}</p>
 			</div>
 		);
 	}
