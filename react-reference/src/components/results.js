cf5a419429a99daab9eb8111e7c290d823d7865d
Rebekah Sunmi Bae <rebekahsunmibae@Rebekahs-MacBook-Pro.local>
2017-10-25 22:51:34 -0400

    Components completed, css, mobile responsive

--- react-reference/src/components/results.js
+++ react-reference/src/components/results.js
@@ -1,16 +1,42 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 import { BrowserRouter, Route, Link, Redirect, Switch } from "react-router-dom";
-import ResultsLog from './resultsLog';
+import ResultsLog from "./resultsLog";
 import "../App.css";
 
-class Results extends Component{
+class Results extends Component {
 	constructor(props) {
 		super(props);
 	}
-render() {
-	return(
-		<div></div>)
-}
+	render() {
+		return (
+			<div className="MainContainer">
+				<div className="NavBar">
+					<div className="NavBarLeft">
+						<p className="WokeSmallText">Woke</p>
+					</div>
+					<div className="NavBarRight">
+						<p className="CurrentLinkText">Search</p>
+						<a href="#">Dashboard</a>
+						<a href="#">Logout</a>
+					</div>
+				</div>
+
+				<div className="MiniContainer">
+					<div className="CurrentSearchHeader">
+						<p className="CurrentSearchText">Relevancy of</p>
+						<p className="CurrentSearchMainText">"SEARCH TERM"</p>
+						<p className="CurrentSearchText">in the past 30 days</p>
+					</div>
+
+					<div className="ResultsContainer">
+						<ResultsLog />
+						<ResultsLog />
+						<ResultsLog />
+					</div>
+				</div>
+			</div>
+		);
+	}
 }
 
 export default Results;
--- react-reference/src/components/resultsLog.js
+++ react-reference/src/components/resultsLog.js
@@ -1,15 +1,36 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 import { BrowserRouter, Route, Link, Redirect, Switch } from "react-router-dom";
 import "../App.css";
 
-class ResultsLog extends Component{
+class ResultsLog extends Component {
 	constructor(props) {
 		super(props);
 	}
-render() {
-	return(
-		<div></div>)
-}
+	render() {
+		return (
+			<div className="ResultsLogContainer">
+				<div className="NewsSource">
+					<p>CNN</p>
+				</div>
+
+				<div className="CurrentResult">
+					<p className="CurrentResultText">Topic: 10,000</p>
+				</div>
+
+				<div className="PrevResult">
+					<p className="ResultText">Topic: 10,000</p>
+				</div>
+
+				<div className="PrevResult">
+					<p className="ResultText">Topic: 10,000</p>
+				</div>
+
+				<div className="PrevResult">
+					<p className="ResultText">Topic: 10,000</p>
+				</div>
+			</div>
+		);
+	}
 }
 
 export default ResultsLog;
