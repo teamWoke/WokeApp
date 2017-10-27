1727fe2bc8bd0edddb9e08dc7247f91e00bd1ae7
Rebekah Sunmi Bae <rebekahsunmibae@Rebekahs-MacBook-Pro.local>
2017-10-27 16:31:26 -0400

    Render dashboard view, attempted delete fn

--- react-reference/src/components/dashboardTile.js
+++ react-reference/src/components/dashboardTile.js
@@ -3,9 +3,9 @@ import "../App.css";
 
 class DashboardTile extends Component {
 	render() {
-		console.log("Rendering from DashboardTile", this.props.tileText);
+		console.log("Rendering from DashboardTile", this.props.id);
 		return (
-			<div className="Tile">
+			<div className="Tile" id={this.props.id} onClick={this.props.onClick}>
 				<p className="TileText">{this.props.tileText}</p>
 			</div>
 		);
